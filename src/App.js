4dd4cf084d79fb0e9d96eaa84a62080796b2e931
rodgc <rod.gc@hotmail.com>
2020-08-27 18:04:42 -0500

    Add Chat component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./sidebar/Sidebar";
+import Chat from "./chat/Chat";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        {/*Chat*/}
+        <Chat />
       </div>
     </div>
   );
---
+++ src/chat/Chat.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
+function Chat() {
+  const [input, setInput] = useState("");
+  const sendMessage = (e) => {
+      e.preventDefault(); 
+  };
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Nmae</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Rodrigo Gonzalez</span>
+          Hey Guys lets do it
+          <span className="chat__timestamp">3:43pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <form>
+          <input
+            tyoe="text"
+            placeholder="Type a message"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" onClick={sendMessage}>
+            {" "}
+            Send a Message
+          </button>
+        </form>
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
