14b9bd72f3b4a38a7b4e2db35210a3170701f6af
rodgc <rod.gc@hotmail.com>
2020-08-27 15:22:55 -0500

    Innitil Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/*Side*/}
+        {/*Chat*/}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNFLFRi35Awu51wiy6jGIC6cIrkS6O_NU",
+    authDomain: "whatsapp-clone-25d92.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-25d92.firebaseio.com",
+    projectId: "whatsapp-clone-25d92",
+    storageBucket: "whatsapp-clone-25d92.appspot.com",
+    messagingSenderId: "148834373161",
+    appId: "1:148834373161:web:5dfd94737329cac24c5638",
+    measurementId: "G-JJFGR8P9KZ"
+  };
